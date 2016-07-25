200e4df76434e7580a610bed6bc188791794470d
ventsy95 <ventsy95@mail.bg>
2016-07-25 23:49:49 +0300

    Demo class created to test both types of notepads and their methods

---
+++ NotepadDemo.java
@@ -0,0 +1,21 @@
+
+public class NotepadDemo {
+
+	public static void main(String[] args) {
+		//SimpleNotepad simple= new SimpleNotepad(2);
+		INotepad secured= new SecuredNotepad(4, "asdf1234");
+	/*	
+		simple.checkOutPages();
+		simple.deleteTextOnPage(2);
+		simple.addTextToPage(1, "bla bla");
+		simple.insertNewTextOnPage(2, "New text");
+		simple.checkOutPages();
+		*/
+		secured.checkOutPages();
+		secured.deleteTextOnPage(3);
+		secured.addTextToPage(4, "Some more text");
+		secured.insertNewTextOnPage(3, "New text on this page");
+		secured.checkOutPages();
+	}
+
+}
