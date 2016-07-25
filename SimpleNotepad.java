1c9da5b7a77ab4db39c5598455b918cc348582a9
ventsy95 <ventsy95@mail.bg>
2016-07-25 23:48:06 +0300

    Verification added for page number to all methods taking it as parameter. Pages index corrected in addTextToPage and insertNewTextOnPage methods

--- SimpleNotepad.java
+++ SimpleNotepad.java
@@ -5,39 +5,47 @@ public class SimpleNotepad implements INotepad {
 
 	public SimpleNotepad(int numberOfPages) {
 		this.pages = new Page[numberOfPages];
-		Scanner sc= new Scanner(System.in);
-		for(int index=0; index<numberOfPages; index++){
-			System.out.println("Enter tytle for page number: "+numberOfPages);
-			String title=sc.nextLine();
-			System.out.println("Enter text for page number: "+numberOfPages);
-			String text=sc.nextLine();
-			this.pages[index]=new Page(title, text);
+		Scanner sc = new Scanner(System.in);
+		for (int index = 0; index < numberOfPages; index++) {
+			System.out.println("Enter title for page number: " + (index+1));
+			String title = sc.nextLine();
+			System.out.println("Enter text for page number: " + (index+1));
+			String text = sc.nextLine();
+			this.pages[index] = new Page(title, text);
 		}
 	}
 
 	@Override
 	public void deleteTextOnPage(int page) {
-		this.pages[page].deleteText();
+		if (page <= this.pages.length && page > 0) {
+			this.pages[page-1].deleteText();
+		}
 	}
 
 	@Override
 	public void checkOutPages() {
-		if (this.pages != null) {
-			for (int index = 0; index < this.pages.length; index++) {
+		for (int index = 0; index < this.pages.length; index++) {
+			if (this.pages[index] != null) {
 				this.pages[index].readPage();
+			} else {
+				System.out.println("Page number: " + (index + 1) + " is empty");
 			}
 		}
 	}
 
 	@Override
 	public void addTextToPage(int page, String text) {
-		this.pages[page].addText(text);
+		if (page <= this.pages.length && page > 0) {
+			this.pages[page-1].addText(text);
+		}
 	}
 
 	@Override
 	public void insertNewTextOnPage(int page, String text) {
-		this.pages[page].deleteText();
-		this.pages[page].addText(text);
+		if (page <= this.pages.length && page > 0) {
+			this.pages[page-1].deleteText();
+			this.pages[page-1].setText(text);
+		}
 	}
 
 }
