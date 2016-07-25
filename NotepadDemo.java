cc39309e467673cbb62f12f91db209f0683d7a1b
ventsy95 <ventsy95@mail.bg>
2016-07-25 23:54:36 +0300

    No code changes, added a few empty lines between code lines.

--- NotepadDemo.java
+++ NotepadDemo.java
@@ -2,15 +2,17 @@
 public class NotepadDemo {
 
 	public static void main(String[] args) {
-		//SimpleNotepad simple= new SimpleNotepad(2);
-		INotepad secured= new SecuredNotepad(4, "asdf1234");
-	/*	
+
+		SimpleNotepad simple = new SimpleNotepad(2);
+
+		INotepad secured = new SecuredNotepad(4, "asdf1234");
+
 		simple.checkOutPages();
 		simple.deleteTextOnPage(2);
 		simple.addTextToPage(1, "bla bla");
 		simple.insertNewTextOnPage(2, "New text");
 		simple.checkOutPages();
-		*/
+
 		secured.checkOutPages();
 		secured.deleteTextOnPage(3);
 		secured.addTextToPage(4, "Some more text");
