6b1ff5e7ff64a2f5c83627ea9ee7bd9b8880aa9e
ventsy95 <ventsy95@mail.bg>
2016-07-26 01:19:13 +0300

    Added more testing for ElectricSecuredNotebook class.

--- NotepadDemo.java
+++ NotepadDemo.java
@@ -6,6 +6,8 @@ public class NotepadDemo {
 		SimpleNotepad simple = new SimpleNotepad(2);
 
 		INotepad secured = new SecuredNotepad(4, "asdf1234");
+		
+		ElectronicSecuredNotepad elNotepad= new ElectronicSecuredNotepad(3, "password");
 
 		simple.checkOutPages();
 		simple.deleteTextOnPage(2);
@@ -18,6 +20,16 @@ public class NotepadDemo {
 		secured.addTextToPage(4, "Some more text");
 		secured.insertNewTextOnPage(3, "New text on this page");
 		secured.checkOutPages();
+		
+		elNotepad.checkOutPages();
+		elNotepad.start();
+		elNotepad.addTextToPage(1, "bla bla bla");
+		elNotepad.deleteTextOnPage(3);
+		elNotepad.printAllPagesWithDigits();
+		elNotepad.searchWord("duma");
+		elNotepad.checkOutPages();
+		elNotepad.stop();
+		elNotepad.deleteTextOnPage(2);
 	}
 
 }
