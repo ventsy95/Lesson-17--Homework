115764d6ed2dc6f676603b8d6b180e577998f696
ventsy95 <ventsy95@mail.bg>
2016-07-26 00:59:29 +0300

    ElectronicSecuredNotepad class created with implemented methods from inherited IElectronicDevices interface; added extra isStarted check to inherited methods from SecuredNotepad class.

---
+++ ElectronicSecuredNotepad.java
@@ -0,0 +1,115 @@
+
+public class ElectronicSecuredNotepad extends SecuredNotepad implements IElectronicDevice {
+	private boolean isNotepadStarted;
+
+	public ElectronicSecuredNotepad(int numberOfPages, String password) {
+		super(numberOfPages, password);
+
+	}
+
+	@Override
+	public void start() {
+		this.isNotepadStarted = true;
+	}
+
+	@Override
+	public void stop() {
+		this.isNotepadStarted = false;
+	}
+
+	@Override
+	public boolean isStarted() {
+		if (isNotepadStarted) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void deleteTextOnPage(int page) {
+		if (isStarted()) {
+			if (passwordCheck() && page <= this.pages.length && page > 0) {
+				this.pages[page - 1].deleteText();
+			}
+		} else {
+			System.out.println("Notepad must first be started.");
+		}
+	}
+
+	@Override
+	public void checkOutPages() {
+		if (isStarted()) {
+			if (passwordCheck()) {
+				if (this.pages != null) {
+					for (int index = 0; index < this.pages.length; index++) {
+						this.pages[index].readPage();
+					}
+				}
+			}
+		} else {
+			System.out.println("Notepad must first be started.");
+		}
+	}
+
+	@Override
+	public void addTextToPage(int page, String text) {
+		if (isStarted()) {
+			if (passwordCheck() && page <= this.pages.length && page > 0) {
+				this.pages[page - 1].addText(text);
+			}
+		} else {
+			System.out.println("Notepad must first be started.");
+		}
+	}
+
+	@Override
+	public void insertNewTextOnPage(int page, String text) {
+		if (isStarted()) {
+			if (passwordCheck() && page <= this.pages.length && page > 0) {
+				this.pages[page - 1].deleteText();
+				this.pages[page - 1].setText(text);
+			}
+		} else {
+			System.out.println("Notepad must first be started.");
+		}
+	}
+
+	@Override
+	public boolean searchWord(String word) {
+		if (isStarted()) {
+			if (passwordCheck()) {
+				for (int index = 0; index < pages.length; index++) {
+					if (pages[index].searchWord(word)) {
+						System.out.println("Word found on page " + (index + 1));
+						return true;
+					}
+				}
+				System.out.println("Word not found.");
+				return false;
+			} else {
+				return false;
+			}
+		} else {
+			System.out.println("Notepad must first be started.");
+			return false;
+		}
+	}
+
+	@Override
+	public void printAllPagesWithDigits() {
+		if (isStarted()) {
+			if (passwordCheck()) {
+				for (int index = 0; index < pages.length; index++) {
+					if (pages[index].containtsDigits()) {
+						pages[index].readPage();
+					}
+				}
+			}
+		} else {
+			System.out.println("Notepad must first be started.");
+		}
+
+	}
+
+}
